002550cea655bb318c3be7f389b713c3ab54f15a
Pouya Rahmani <r.pouya84@gmail.com>
2024-05-09 23:44:40 +0330

    pp1

--- src/main/java/com/ikiu/loginjfx/sceneApplication.java
+++ src/main/java/com/ikiu/loginjfx/sceneApplication.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 // no heloooo
+//feiekdkdkeidkdkeidkeeieddeee
 
 public class sceneApplication extends Application {
 
